e895c143fb50a5276dbd2cc5634d17234fddcce6
Artbycat <64057988+Artbycat@users.noreply.github.com>
2020-07-09 22:55:11 -0500

    Update Desafio0202.cs

--- Desafio0202.cs
+++ Desafio0202.cs
@@ -6,18 +6,22 @@ namespace Desafío0202
     {
         static void Main(string[] args)
         {
-
+            //input del usuario
             Console.WriteLine("Para hallar los catetos y el ángulo interno restante de un triángulo rectángulo:");
             Console.Write("Ingrese T: ");
             double t = double.Parse(Console.ReadLine());
 
             Console.Write("Ingrese a: ");
             double a = double.Parse(Console.ReadLine());
-
+            
+            //conversion a radianes
             double aRad = a * Math.PI / 180;
+            
+            //Catetos
             double y = t * Math.Sin(aRad);
             double z = (t * Math.Cos(aRad));
 
+            //ángulo interno
             double c = 90 - a;
 
             Console.WriteLine("y: " + y);
