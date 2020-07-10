06a3118bfde0ce936b201485605851796bbfe1a7
Artbycat <64057988+Artbycat@users.noreply.github.com>
2020-07-09 22:52:43 -0500

    Update Desafio0201.cs

--- Desafio0201.cs
+++ Desafio0201.cs
@@ -6,18 +6,20 @@ namespace Desafio0201
     {
         static void Main(string[] args)
         {
+            //Input del usuario
             Console.WriteLine("Para hallar la hipotenusa y los ángulos internos de un triángulo rectángulo:");
             Console.Write("Ingrese Y: ");
             double y = double.Parse(Console.ReadLine());
 
             Console.Write("Ingrese Z: ");
             double z = double.Parse(Console.ReadLine());
-
+            
+            //hipotenusa
             double t = (y) * (y) + (z) * (z);
             double T = Math.Sqrt(t);
         
             Console.WriteLine("La hipotenusa del triángulo es: " + T);
-
+            //ángulos internos
             double c = Math.Atan((z) / (y));
             double cGrados = c * (180 / Math.PI);
             double a = Math.Atan((y) / (z));
