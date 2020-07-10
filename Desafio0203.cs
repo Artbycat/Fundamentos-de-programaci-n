02c2d87f3a23781ed84be72f3163265e4b897443
Artbycat <64057988+Artbycat@users.noreply.github.com>
2020-07-09 22:49:20 -0500

    Add files via upload

---
+++ Desafio0203.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Desafío0203
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Input del usuario
+            Console.WriteLine("Para hallar un cateto, la hipotenusa y el ángulo interno restante de un triángulo rectángulo:");
+            Console.Write("Ingrese Z: ");
+            double z = double.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese c: ");
+            double c = double.Parse(Console.ReadLine());
+
+            //operaciones matemáticas
+            //Angulo
+            double a = 90 - c;
+            //Conversion a radianes
+            double cRad = c * Math.PI / 180;
+            double aRad = a * Math.PI / 180;
+            //hallar cateto, hipotenusa.
+            double y = (z * Math.Sin(aRad)) / (Math.Sin(cRad));
+            double t = (y) / (Math.Sin(aRad));
+            
+
+            //consola shows
+            Console.WriteLine("Hipotenusa: " + t);
+            Console.WriteLine("y: " + y);
+            Console.WriteLine("El ángulo interno restante es: " + a);
+        }
+    }
+}
