844743097a89da7624d744f14edc9ecf088bd985
Artbycat <64057988+Artbycat@users.noreply.github.com>
2020-07-16 20:44:01 -0500

    Add files via upload

---
+++ Caja compensación.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Caja_compensación
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Input del usuario
+            Console.WriteLine("Para saber su tarifa en 2020:");
+            Console.WriteLine("Ingrese su salario: ");
+            int s = int.Parse(Console.ReadLine());
+
+            //Cálculo
+            double smmlv = s / 877803;
+
+            if (smmlv <= 2)
+            {
+                Console.WriteLine("Tarifa A");
+            }
+            else if (2 < smmlv && smmlv <= 4)
+            {
+                Console.WriteLine("Tarifa B");
+            }
+            else
+            {
+                Console.WriteLine("Tarifa C");
+            }
+        }
+    }
+}
