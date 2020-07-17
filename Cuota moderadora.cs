7f700a907a92db108a2889c919b34d7c4ac860d1
Artbycat <64057988+Artbycat@users.noreply.github.com>
2020-07-16 21:35:11 -0500

    Add files via upload

---
+++ Cuota moderadora.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Cuota_moderadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Input del usuario
+            Console.WriteLine("Para saber su cuota moderadora en 2020:");
+            Console.WriteLine("Ingrese su salario: ");
+            int s = int.Parse(Console.ReadLine());
+
+            //Cálculo
+            double smmlv = s / 877803;
+
+            if (smmlv <= 2)
+            {
+                Console.WriteLine("Cuota A: usted debe pagar $3.400");
+            }
+            else if (2 < smmlv && smmlv <= 5)
+            {
+                Console.WriteLine("Cuota B: usted debe pagar $13.500");
+            }
+            else
+            {
+                Console.WriteLine("Cuota C: usted debe pagar $35.600");
+            }
+        }
+    }
+}
