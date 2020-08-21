ab881e513411d24edce83ada47037283f33a079a
Artbycat <64057988+Artbycat@users.noreply.github.com>
2020-08-21 00:06:28 -0500

    Add files via upload

---
+++ Blackjack Arreglos.cs
@@ -0,0 +1,117 @@
+﻿using System;
+
+namespace Blackjack_Arreglos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int carta = 2, total = 0, puntuacion = 0, jugador = 1, jugadores = 0, ganador = 0, jugadorWin = 0, jugadoresAct = 0;
+            Random aleatorio = new Random();
+            string cont = "";
+
+            Console.WriteLine("Ingrese el número de jugadores: ");
+            jugadores = int.Parse(Console.ReadLine());
+            jugadoresAct = jugadores;
+            string[] nombres = new string[jugadores];
+            int[] puntuacion1 = new int[jugadores];
+
+            while (jugadores < 2 || jugadores > 5)
+            {
+                Console.WriteLine("Ingrese un número válido de jugadores.");
+                jugadores = int.Parse(Console.ReadLine());
+            }
+            
+            for ( int i = 0; i< nombres.Length; i++)
+            {
+                Console.WriteLine("Ingrese su nombre: ");
+                nombres[i] = Console.ReadLine();
+            }
+            for (int i = 0; i < nombres.Length; i++)
+            {
+                while (jugador < jugadores + 1)
+                {
+                    Console.WriteLine("¿Iniciar el juego? (s/n)");
+                    cont = Console.ReadLine();
+                    while (cont != "s" && cont != "n")
+                    {
+                        Console.WriteLine("Ingrese una entrada válida.");
+                        cont = Console.ReadLine();
+                    }
+                    Console.WriteLine("Jugador: " + jugador);
+                    puntuacion = aleatorio.Next(1, 21);
+                    total = puntuacion;
+                    Console.WriteLine("Cartas: " + carta + "  Puntuación: " + puntuacion + "  Total: " + total);
+                    while (cont == "s")
+                    {
+                        puntuacion = aleatorio.Next(1, 11);
+                        Console.WriteLine("¿Continuar? (s/n)");
+                        cont = Console.ReadLine();
+                        while (cont != "s" && cont != "n")
+                        {
+                            Console.WriteLine("Ingrese una entrada válida.");
+                            cont = Console.ReadLine();
+                        }
+                        if (cont == "s")
+                        {
+                            carta = carta++;
+                            total += puntuacion;
+                            Console.WriteLine("Cartas: " + carta + "  Puntuación: " + puntuacion + "  Total: " + total);
+                        }
+                        if (total > 21)
+                        {
+                            total = 0;
+                            Console.WriteLine("Ha perdido el juego.");
+                            cont = "n";
+                        }
+                        else if (total == 21)
+                        {
+                            Console.WriteLine("Ha ganado el juego.");
+                            cont = "n";
+                        }
+                        else if (cont == "n")
+                        {
+                            Console.WriteLine("Gracias por jugar.");
+
+                        }
+                        Console.WriteLine("Su total fue de: " + total);
+                    }
+                    jugador += 1;
+                    if (total > ganador)
+                    {
+                        ganador = total;
+                        if (jugador > jugadorWin)
+                        {
+                            jugadorWin = jugador;
+                        }
+                    }
+                    if (jugador >= jugadores) break;
+                }       
+            }
+
+            for (int i = 0; i < nombres.Length; i++)
+            {
+                Console.WriteLine(nombres[i] + ": " + puntuacion1[i]);
+            }
+
+            for (int j = 0; j < nombres.Length - 1; j++)
+            {
+                for (int i = 0; i < nombres.Length - 1; i++)
+                {
+                    if (puntuacion1[i] < puntuacion1[i + 1])
+                    {
+                        int temp = puntuacion1[i];
+                        puntuacion1[i] = puntuacion1[i + 1];
+                        puntuacion1[i + 1] = temp;
+
+                        string temp1 = nombres[i];
+                        nombres[i] = nombres[i + 1];
+                        nombres[i + 1] = temp1;
+                    }
+                }
+            }
+            Console.WriteLine("El ganador es: " + nombres[0]);
+            Console.WriteLine("El segundo lugar es:  " + nombres[1]);
+        }
+    }    
+}
---
+++ Promedios estatura.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Promedios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] nombres = { "Tanjiro", "Nezuko", "Zenitsu", "Inosuke", "Genya", "Kanao", "Tomioka" };
+            double[] estatura = { 165, 153, 164.5, 164, 180, 156, 176 };
+            double[] dist = new double[7];
+
+            double total = 0;
+            total += estatura[0];
+            total += estatura[1];
+            total += estatura[2];
+            total += estatura[3];
+            total += estatura[4];
+            total += estatura[5];
+            total += estatura[6];
+            double promedio = total / 7;
+            Console.WriteLine("El promedio de las estaturas es:  " + promedio);
+
+            for (int i = 0; i < dist.Length; i++)
+            {
+                dist[i] = Math.Sqrt((estatura[i] - promedio) * (estatura[i] - promedio)) + (estatura[i] - promedio) * (estatura[i] - promedio);
+            }
+
+            double totalDist = 0;
+            totalDist += dist[0];
+            totalDist += dist[1];
+            totalDist += dist[2];
+            totalDist += dist[3];
+            totalDist += dist[4];
+            totalDist += dist[5];
+            totalDist += dist[6];
+            double promedioDist = totalDist/ 7;
+            Console.WriteLine("El promedio de las distancias entre las estaturas y el promedio de estaturas es: " + promedioDist);
+        }
+    }
+}
